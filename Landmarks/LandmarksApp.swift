38ad60cffd8f40b77c00ae5b8f8a52ca57ee1836
Vitor <reikodeveloper@gmail.com>
2023-07-22 16:53:06 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Vitor Lucas Pires Cordovil on 22/07/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
